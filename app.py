9aac808dedeb574892e087a28bdec5a3de6f64b7
greensun2001 <dbc2001@gmail.com>
2025-01-19 14:19:50 +0700

    update app.py - remove git link

--- app.py
+++ app.py
@@ -132,18 +132,13 @@ def check_password():
 st.set_page_config(
     page_title="HU-UTC Shear Program",
 )
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 if not check_password():
     st.stop()
 
